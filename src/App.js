31e1989bb243bb9d22800eb0a0973e100940b9cf
Renato Silva <E1412@itsector.pt>
2024-03-12 17:03:08 +0000

    16. State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import "./App.css";
 import React from "react";
 class App extends React.Component {
   state = {
-    name: "Renato Gama",
-    counter: 0,
+    posts: [
+      { id: 1, title: "O titulo 1", body: "O corpo 1" },
+      { id: 2, title: "O titulo 2", body: "O corpo 2" },
+      { id: 1, title: "O titulo 2", body: "O corpo 2" },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
