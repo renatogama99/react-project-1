0acc8c30d099de05f4924779889405119a432871
Renato Silva <E1412@itsector.pt>
2024-03-12 17:23:59 +0000

    17. & 18. - Lifecycle methods

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import "./App.css";
 import React from "react";
 class App extends React.Component {
   state = {
+    counter: 0,
     posts: [
       { id: 1, title: "O titulo 1", body: "O corpo 1" },
       { id: 2, title: "O titulo 2", body: "O corpo 2" },
-      { id: 1, title: "O titulo 2", body: "O corpo 2" },
+      { id: 3, title: "O titulo 3", body: "O corpo 3" },
     ],
+  };
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+
+    posts[0].title = "O titulo foi alterado";
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 2000);
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
